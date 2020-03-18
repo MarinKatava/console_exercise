6ecd3621e2ab8d69dcdb9efab34356c9025ae896
Marin <katava.marin@nsoft.com>
2020-03-18 21:50:22 +0100

    initial structure

---
+++ src/main/java/com/cgm/consoleexercise/ConsoleExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.cgm.consoleexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsoleExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgm/consoleexercise/ConsoleExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgm.consoleexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsoleExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
